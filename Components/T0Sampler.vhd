11e0b37ea998257fb446ed5d57aad6f80c4231a7
singaputih <singaputih257@gmail.com>
2024-10-28 23:13:10 -0400

    Updated T0Sampler

--- Components/T0Sampler.vhd
+++ Components/T0Sampler.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,6 +33,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity T0Sampler is
+        
     Port ( vpeClean : in STD_LOGIC;
            frameStartEn : out STD_LOGIC;
            wordStartEn : out STD_LOGIC;
@@ -57,29 +59,72 @@ signal t0PushEn: std_logic;
 begin
 
 
-    Sample_Controller: process(clock, reset)
+    SAMPLE_CONTROLLER: process(clock, reset)
     variable risingFlag: std_logic := not ACTIVE;
     begin
-    if(reset = ACTIVE) then
-        risingFlag := not ACTIVE;
-        sampleState <= LOWCOUNT;
-        lowCountEn <= not ACTIVE;
-        highCountEn <= not ACTIVE;
-    elsif(rising_edge (clock)) then
-        case (SampleState) is
-        
-        when LOWCOUNT =>
-            if(vpeClean = not risingFlag) then
-                sampleState <= HIGHCOUNT;
-                lowCountEn <= ACTIVE;
+        if(reset = ACTIVE) then
+            risingFlag := not ACTIVE;
+            sampleState <= LOWCOUNT;
+            lowCountEn <= not ACTIVE;
+            highCountEn <= not ACTIVE;
+        elsif(rising_edge (clock)) then
+            case (SampleState) is
+            
+            --LOWCOUNT State, Will count in Background until vpeClean goes HIGH
+            --Afterward, will send lowCountEn to stop count and Transition into HIGHCOUNT
+            when LOWCOUNT =>
+                if(vpeClean = not risingFlag) then
+                    sampleState <= HIGHCOUNT;
+                    lowCountEn <= ACTIVE;
+                end if;
+            
+            --HIGHCOUNT, will count in Background after vpeClean goes HIGH
+            --Will send highCountEn to stop count when vpeClean goes low and transition
+            --into CALCULATE
+            when HIGHCOUNT =>
+                if(vpeClean = not risingFlag) then
+                    sampleState <= CALCULATE;
+                    highCountEn <= ACTIVE;
+                end if;
+                
+            --will calculate t0 and receive t0PushEn when done, transitions back into LOWCOUNT
+            when CALCULATE =>
+                if(t0PushEn = ACTIVE) then
+                    sampleState <= LOWCOUNT;
+                end if;
+            end case;
+        end if;
+    end process;
+    
+    LOW_COUNT: process(clock, reset)
+    variable lowClock: integer;
+    begin
+        if(reset = ACTIVE) then
+            lowTime <= 0;
+        elsif(rising_edge (clock)) then
+            if(lowCountEn = ACTIVE) then
+                lowTime <= lowClock;
+                lowClock := 0;
+            else
+                lowClock := lowClock + 1;    
             end if;
-        when HIGHCOUNT =>
-            if(vpeClean = not risingFlag) then
-                sampleState <= CALCULATE;
-                highCountEn <= ACTIVE;
+        end if;
+    end process;
+    
+    HIGH_COUNT: process(clock, reset)
+    variable highClock: integer;
+    begin
+        if(reset = ACTIVE) then
+            highTime <= 0;
+        elsif(rising_edge (clock)) then
+            if(highCountEn = ACTIVE) then
+                highTime <= highClock;
+                highClock := 0;
+            else
+                highClock := highClock + 1;    
             end if;
-        when CALCULATE =>
-            if(t0PushEn = ACTIVE) then
-                sampleState <= LOWCOUNT;
-            end if;
+        end if;
+    end process;
+    
+    
 end T0Sampler_ARCH;
